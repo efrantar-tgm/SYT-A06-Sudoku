f0def086ba993686c07f20a7d35d8f966a876aed
efrantar-tgm <efrantar@student.tgm.ac.at>
2013-11-11 09:53:41 +0100

    added normal solver

---
+++ sudokuSolvers.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/*
+ * From: https://github.com/AntonFagerberg/Sudoku-C/blob/master/sudoku.c
+ * Sudoku Solver (brute-force).
+ * with the purpose of beeing fast as hell. It's pretty fast.
+ * 2012 Anton Fagerberg [anton at antonfagerberg dot com]
+ * wwww.antonfagerberg.com
+ */
+uint8_t solve(uint8_t i) {
+        // Solved it!
+        if (!(i ^ 81))
+                return 1;
+
+        // Skip the positions which are defined from the start.
+        if (puzzle[i]) 
+                return solve(i + 1);
+
+        uint8_t align = i % 9;
+        uint16_t valid_numbers = puzzle[ align ];
+        valid_numbers = valid_numbers | puzzle[ align + 9 ];
+        valid_numbers = valid_numbers | puzzle[ align + 18 ];
+        valid_numbers = valid_numbers | puzzle[ align + 27 ];
+        valid_numbers = valid_numbers | puzzle[ align + 36 ];
+        valid_numbers = valid_numbers | puzzle[ align + 45 ];
+        valid_numbers = valid_numbers | puzzle[ align + 54 ];
+        valid_numbers = valid_numbers | puzzle[ align + 63 ];
+        valid_numbers = valid_numbers | puzzle[ align + 72 ];
+
+        align = i / 9 * 9;
+        valid_numbers = valid_numbers | puzzle[ align ];
+        valid_numbers = valid_numbers | puzzle[ align + 1 ];
+        valid_numbers = valid_numbers | puzzle[ align + 2 ];
+        valid_numbers = valid_numbers | puzzle[ align + 3 ];
+        valid_numbers = valid_numbers | puzzle[ align + 4 ];
+        valid_numbers = valid_numbers | puzzle[ align + 5 ];
+        valid_numbers = valid_numbers | puzzle[ align + 6 ];
+        valid_numbers = valid_numbers | puzzle[ align + 7 ];
+        valid_numbers = valid_numbers | puzzle[ align + 8 ];
+        
+        // Check "squares"
+        align = 27 * (i / 9 / 3) + 3 * (i / 3 % 3);
+        valid_numbers = valid_numbers | puzzle[ align ];
+        valid_numbers = valid_numbers | puzzle[ align + 9 ];
+        valid_numbers = valid_numbers | puzzle[ align + 18 ];
+
+        valid_numbers = valid_numbers | puzzle[ align + 1 ];
+        valid_numbers = valid_numbers | puzzle[ align + 10 ];
+        valid_numbers = valid_numbers | puzzle[ align + 19 ];
+        
+        valid_numbers = valid_numbers | puzzle[ align + 2 ];
+        valid_numbers = valid_numbers | puzzle[ align + 11 ];
+        valid_numbers = valid_numbers | puzzle[ align + 20 ];
+
+        uint8_t next = i + 1;
+        if (!(valid_numbers & 1)) {
+                puzzle[i] = 1;
+                if (solve(next))
+                        return 1;
+        }
+        if (!(valid_numbers & 2)) {
+                puzzle[i] = 2;
+                if (solve(next))
+                        return 1;
+        }
+        if (!(valid_numbers & 4)) {
+                puzzle[i] = 4;
+                if (solve(next))
+                        return 1;
+        }
+        if (!(valid_numbers & 8)) {
+                puzzle[i] = 8;
+                if (solve(next))
+                        return 1;
+        }
+        if (!(valid_numbers & 16)) {
+                puzzle[i] = 16;
+                if (solve(next))
+                        return 1;
+        }
+        if (!(valid_numbers & 32)) {
+                puzzle[i] = 32;
+                if (solve(next))
+                        return 1;
+        }
+        if (!(valid_numbers & 64)) {
+                puzzle[i] = 64;
+                if (solve(next))
+                        return 1;
+        }
+        if (!(valid_numbers & 128)) {
+                puzzle[i] = 128;
+                if (solve(next))
+                        return 1;
+        }
+        if (!(valid_numbers & 256)) {
+                puzzle[i] = 256;
+                if (solve(next))
+                        return 1;
+        }
+
+        return puzzle[i] = 0;
+}
+
